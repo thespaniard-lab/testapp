db475a0250df53d491ba5c96222abe0dafa02de9
thespaniard-lab <massimo.schianchi@licensespring.com>
2025-07-14 14:28:12 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-#import time
-#
-#print("Hello from ArgoCD deployed Python app!")
-#print("Staying alive to avoid CrashLoopBackOff...")
-#
-# Keep the container running
-#while True:
-#    time.sleep(60)
+import time
+
+print("Hello from ArgoCD deployed Python app!")
+print("Staying alive to avoid CrashLoopBackOff...")
+
+#Keep the container running
+while True:
+    time.sleep(60)
